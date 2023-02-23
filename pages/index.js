bfba80bb78887dbe249c79acce17aff2b53e3f89
Shairyar Baig <shairyarbaig@gmail.com>
2023-02-23 15:03:05 +0500

    Add couple of routes/pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
@@ -11,7 +13,9 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
+          <div>Read <Link href="/posts/first-post">this page!</Link></div>
+
         </h1>
 
         <p className={styles.description}>
